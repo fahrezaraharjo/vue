1b217cfb2c74c01ec50273ac5fbc4a9698209ab0
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-04-03 21:15:06 +0700

    initial project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RubiMap msg="Rubi Henjaya"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import RubiMap from './components/RubiMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    RubiMap
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
